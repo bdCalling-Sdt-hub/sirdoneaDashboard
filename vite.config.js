9273390de8773b96ff70336f929d7306f892d1da
humayonitbd <humayonforid44@gmail.com>
2025-02-02 18:08:20 +0600

    updated run on ip

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-      port: 3002
-    }
-
+  server: {
+    port: 3002,
+    host: '0.0.0.0', // Allows access from any IP
+    open: true // Optional: Automatically open in browser
+  }
   
 })
