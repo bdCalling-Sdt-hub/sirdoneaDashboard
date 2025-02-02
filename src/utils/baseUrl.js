db88fb7280c1e0c785bc81597b916a2e21c045a1
humayonitbd <humayonforid44@gmail.com>
2025-02-02 17:43:05 +0600

    dashboard updated

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 export const getBaseUrl = () => {
-  // return "http://192.168.10.176:3000/api/v1";
-  return "http://10.0.70.35:8010/api/v1";
+  return "http://139.59.0.25:8050/api/v1";
+  // return "http://10.0.70.35:8010/api/v1";
 };
